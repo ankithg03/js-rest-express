983ba00a3e2e98164379b7ab66f2cb55799baf94
Ankith G <corra-337@Corra-337.local>
2023-09-22 10:54:41 +0530

    Added the image attributes and data update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors');
+
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+app.use(cors());
 
 /**
  * /signup rest API
